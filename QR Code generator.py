408df96b7f9f358a3903ead1f724874277e19da5
vipin956 <168722955+vipin956@users.noreply.github.com>
2024-07-26 14:28:56 +0530

    Update QR Code generator.py

--- QR Code generator.py
+++ QR Code generator.py
@@ -3,4 +3,4 @@ import pyqrcode  #pyqrcode =  it is used for qr code generation
 url = input("Enter a URL to generate QR code: \n") #enter any url from web
 
 QR = pyqrcode.create(url) # url creation 
-QR.png("webqr.png", scale=8)  #scale is ued for parameter
+QR.png("webqr.png", scale=8)  #scale is used for parameter
